2f94e36691f4d84905b5127e08c0a1a9492724b4
Rajiv Venugopal <rajivgopal@yahoo.com>
2019-08-10 10:41:12 +0800

    Updates

---
+++ precache-manifest.90ee4ce9db601383b8598648edd7b398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b866b23228c2b79b501a9950faeb8d",
+    "url": "/contact-manager-app/index.html"
+  },
+  {
+    "revision": "a181b76dac521ad0bce8",
+    "url": "/contact-manager-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d3f18de66128c8e778b8",
+    "url": "/contact-manager-app/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a181b76dac521ad0bce8",
+    "url": "/contact-manager-app/static/js/2.c38ab4b0.chunk.js"
+  },
+  {
+    "revision": "d3f18de66128c8e778b8",
+    "url": "/contact-manager-app/static/js/main.ea17c2dd.chunk.js"
+  },
+  {
+    "revision": "57bf790c05247af2e9ed",
+    "url": "/contact-manager-app/static/js/runtime~main.74254436.js"
+  }
+]);
